ef1118ad2ae60e5aa58568bac066c6451e0f1d4a
krajcovic46 <stanlezzz@gmail.com>
2015-10-31 10:53:18 +0100

    add the helper scripts

---
+++ hlpr_scrpts.py
@@ -0,0 +1,7 @@
+# Use to
+def update_max():
+    with open("words.txt") as f:
+        for indx, wrd in enumerate(f):
+            pass
+    with open("last_mx_vl.txt", 'w') as f:
+        f.write(index)
