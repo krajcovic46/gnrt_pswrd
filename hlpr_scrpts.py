4431d52ec21d781f1c15e638a722d4ed20e59c6b
krajcovic46 <stanlezzz@gmail.com>
2015-10-31 13:06:14 +0100

    clarify the script

--- hlpr_scrpts.py
+++ hlpr_scrpts.py
@@ -1,4 +1,4 @@
-# Use to
+# Use to update the max value which is used to determine the range from which the true random numbers are picked
 def update_max():
     with open("words.txt") as f:
         for indx, wrd in enumerate(f):
